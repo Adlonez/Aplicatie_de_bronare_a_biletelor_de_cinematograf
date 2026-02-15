d82858d6b962848acd8ad2fb357f0883a8d23d98
Adlonez <lozanadelina90@gmail.com>
2026-02-15 22:02:29 +0200

    added filters to carousel

--- Front/src/pages/Home.tsx
+++ Front/src/pages/Home.tsx
@@ -1,25 +1,38 @@
-import React from 'react'
-import ClickableCarousel from '../components/dataDisplay/Carousel'
-import CinemaCarousel from '../components/MiniCarousel'
-import films from "../_mock/films.json"
-import news from "../_mock/news.json"
-import CinemaNews from '../components/dataDisplay/CinemaNews'
+import React, { useMemo } from "react";
+import ClickableCarousel from "../components/dataDisplay/Carousel";
+import CinemaCarousel from "../components/MiniCarousel";
+import filmsData from "../_mock/films.json";
+import newsData from "../_mock/news.json";
+import CinemaNews from "../components/dataDisplay/CinemaNews";
+import type { Films } from "../types/ui";
 
-const Home = () => {
-  const InProgressitems:any=films
-  const ComingSoonitems:any=films
-  const News:any=news
+const Home: React.FC = () => {
+  const films = filmsData as Films[];
+  const news = newsData as any[]; // ideally type this too later
+
+  const inProgressItems = useMemo(
+    () => films.filter((film) => film.status === "progress"),
+    [films]
+  );
+
+  const comingSoonItems = useMemo(
+    () => films.filter((film) => film.status === "soon"),
+    [films]
+  );
+
+  const topTierItems = useMemo(
+    () => films.filter((film) => film.toptier),
+    [films]
+  );
+
   return (
-   
     <div>
-      <ClickableCarousel/>
-      <CinemaCarousel title="In progress" items={ InProgressitems }/>
-      <CinemaCarousel title="Coming Soon" items={ ComingSoonitems }/>
-      <CinemaNews title="News" items={News} ></CinemaNews>
+      <ClickableCarousel />
+      <CinemaCarousel title="In progress" items={inProgressItems} />
+      <CinemaCarousel title="Coming Soon" items={comingSoonItems} />
+      <CinemaNews title="News" items={news} />
     </div>
-    
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
